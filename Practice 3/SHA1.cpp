2bbd3381cb66a611f3557deaa683c2b01094a907
NikitaMasych <92444221+NikitaMasych@users.noreply.github.com>
2022-06-10 00:27:45 +0300

    Update SHA1.cpp

--- Practice 3/SHA1.cpp
+++ Practice 3/SHA1.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
 #include <iomanip>
-#include <algorithm>
 #include "SHA1.h"
 
 
-uint_fast32_t binaryRepresentation(std::vector<unsigned char> str){
-    // converts 4 symbol string to 32 binary
-    return (int(str[0]) << 24) | (int(str[1]) << 16) | (int(str[2]) << 8) | int(str[3]);
-};
-
 uint_fast32_t leftRotate(uint_fast32_t a, unsigned int c){
-    // implements c bites circular left shift for value a
+    // implements c bits circular left shift for value a
     unsigned int INT_BITS = 32;
     return (a << c)|(a >> (INT_BITS - c));
 }
@@ -21,43 +15,55 @@ std::vector<unsigned char> slice(std::vector<unsigned char> const &v,
     auto first = v.cbegin() + m;
     auto last =  v.cbegin() + n;
 
-    std::vector<unsigned char> vec(first, last);
-    return vec;
+    std::vector<unsigned char> subvector(first, last);
+    return subvector;
 }
 
-std::string stringRepresentation(uint_fast32_t a){
+uint_fast32_t binaryNumber(std::vector<unsigned char> str){
+    // converts 4 character string to 32 binary
+    return (int(str[0]) << 24) | (int(str[1]) << 16) | (int(str[2]) << 8) | int(str[3]);
+};
+
+std::string binaryString(uint_fast64_t a){
+    // converts 64 binary to 8 character string
+    std::string str = "";
+    for (size_t shift = 56; shift != -8; shift -=8 )
+        str += static_cast<char>( (a >> shift) & 0xFF);
+    return str;
+}
+
+std::string hexString(uint_fast32_t a){
     // converts 32 binary to hexadecimal string
-    uint_fast32_t completeByte = 0xFF;
 
     std::stringstream stream;
-    stream << std::setw(2) << std::setfill('0') << std::hex << ((a >> 24) & completeByte);
-    stream << std::setw(2) << std::setfill('0') << std::hex << ((a >> 16) & completeByte);
-    stream << std::setw(2) << std::setfill('0') << std::hex << ((a >> 8)  & completeByte);
-    stream << std::setw(2) << std::setfill('0') << std::hex << ( a        & completeByte);
+    // with respect to leading zeros
+    stream << std::setw(8) << std::setfill('0') << std::hex << a;
 
-    std::string res = stream.str();
-    std::transform(res.begin(), res.end(), res.begin(), ::toupper);
-    return res;
+    std::string str = stream.str();
+
+    return str;
 }
 
 std::string hashLinker(uint_fast32_t h0, uint_fast32_t h1,
                        uint_fast32_t h2, uint_fast32_t h3,
                        uint_fast32_t h4){
-    // generates hexadecimal string as hashing result
-    std::string res = "";
-    res += stringRepresentation(h0);
-    res += stringRepresentation(h1);
-    res += stringRepresentation(h2);
-    res += stringRepresentation(h3);
-    res += stringRepresentation(h4);
-    return res;
+    // concatenates hashes into hexadecimal-represented string
+    std::string str = "";
+
+    str += hexString(h0);
+    str += hexString(h1);
+    str += hexString(h2);
+    str += hexString(h3);
+    str += hexString(h4);
+
+    return str;
 }
 
-std::vector <uint_fast32_t> SHA1::wordsGenerator(const size_t& pos){
+std::vector <uint_fast32_t> SHA1::messageSchedule(const size_t& pos){
     std::vector <uint_fast32_t> words;
     // get 16 words 4 character each = 32 bites
     for(size_t i = 0; i != 64; i += 4){
-        uint_fast32_t word = binaryRepresentation(slice(value, pos+i, pos+i+4));
+        uint_fast32_t word = binaryNumber(slice(value, pos+i, pos+i+4));
         words.push_back(word);
     }
     // get another 64 words based on those 16
@@ -68,11 +74,13 @@ std::vector <uint_fast32_t> SHA1::wordsGenerator(const size_t& pos){
     return words;
 }
 
-void processVals(uint_fast32_t& a, uint_fast32_t& b,
+void conductVals(uint_fast32_t& a, uint_fast32_t& b,
                  uint_fast32_t& c, uint_fast32_t& d,
                  uint_fast32_t& e, const std::vector <uint_fast32_t> &words){
+    // conducts values through loop
+    uint_fast32_t k, f, temp;
+
     for (size_t i = 0; i != 80; ++i){
-        uint_fast32_t k, f;
         if (i < 20){
             f = (b & c) | ((~b) & d);
             k = 0x5A827999;
@@ -90,21 +98,20 @@ void processVals(uint_fast32_t& a, uint_fast32_t& b,
             k = 0xCA62C1D6;
         }
 
-        uint_fast32_t temp = leftRotate(a, 5) + f + e + k + words[i];
+        temp = leftRotate(a, 5) + f + e + k + words[i];
         e = d;
         d = c;
         c = leftRotate(b, 30);
         b = a;
         a = temp;
     }
-
 }
 
-void SHA1::HashForChunk(uint_fast32_t& h0, uint_fast32_t&  h1,
+void SHA1::hashForChunk(uint_fast32_t& h0, uint_fast32_t&  h1,
                         uint_fast32_t& h2, uint_fast32_t&  h3,
                         uint_fast32_t& h4, const size_t& pos){
     // break chunk into 16 + 64 words:
-    std::vector <uint_fast32_t> words = wordsGenerator(pos);
+    std::vector <uint_fast32_t> words = messageSchedule(pos);
 
     // initialize hash values for this chunk:
     uint_fast32_t a, b, c, d, e;
@@ -114,47 +121,37 @@ void SHA1::HashForChunk(uint_fast32_t& h0, uint_fast32_t&  h1,
     d = h3;
     e = h4;
 
-    processVals(a, b, c, d, e, words);
+    conductVals(a, b, c, d, e, words);
 
-    h0 = h0 + a;
-    h1 = h1 + b;
-    h2 = h2 + c;
-    h3 = h3 + d;
-    h4 = h4 + e;
-        
+    // add this chunk's hash to result so far:
+    h0 += a;
+    h1 += b;
+    h2 += c;
+    h3 += d;
+    h4 += e;
+
 }
 
-std::string stringRepresentation2(uint_fast32_t a){
-    // converts 32 binary to 4 character string
-    uint_fast32_t completeByte = 0xFF;
-    std::string res = "";
-    res += static_cast<char>( (a >> 24) & completeByte);
-    res += static_cast<char>( (a >> 16) & completeByte);
-    res += static_cast<char>( (a >> 8)  & completeByte);
-    res += static_cast<char>(  a        & completeByte);
-    return res;
-}
-
-void SHA1::generateLastChunk(){
-    // as far as we storing value in characters, which are one byte
-    // adding 1 is possible only as whole 10000000
+void SHA1::messageProcessing(){
+    // due to storing value in chars, which are one byte
+    // adding 1 is possible only as whole 0b10000000
     value.push_back(static_cast<unsigned char>(0b10000000));
 
     size_t zerosAmount;
-    size_t non512Characters = value.size() % 64;
+    size_t non512Characters = value.size() % 64; // including "0b10000000"
 
-    if (non512Characters <= 56) zerosAmount = 448 - non512Characters * 8; // including "0b10000000"
-    else if (non512Characters < 64) zerosAmount = 512 - (non512Characters) * 8 + 512 - 64;
-    else zerosAmount = (64 - non512Characters - 1) * 8 + (512 - 64);
+    // if enough space without additional block
+    if (non512Characters <= 56) zerosAmount = 448 - non512Characters * 8;
+    // if additional block required
+    else zerosAmount = 512 - (non512Characters) * 8 // in penultimate
+                     + 512 - 64;                    // in additional
 
-    for (size_t i = 0; i < zerosAmount; i += 8)
+    for (size_t i = 0; i != zerosAmount; i += 8)
         value.push_back(static_cast<unsigned char>(0b00000000));
 
     // 64 bit length of the initial message
-    uint_fast64_t len = (value.size()-1) * 8 - zerosAmount; // in bits
-    uint_fast32_t fourByte = 0xFFFFFFFFFFFFFFFF;
-    std::string l = stringRepresentation2((len >> 32) & fourByte) +
-                    stringRepresentation2(len & fourByte);
+    uint_fast64_t len = (value.size() - 1) * 8 - zerosAmount; // in bits
+    std::string l = binaryString(len);
     for (unsigned char c: l) value.push_back(c);
 }
 
@@ -166,13 +163,12 @@ void SHA1::calculateHash(){
     h3 = 0x10325476;
     h4 = 0xC3D2E1F0;
 
-    size_t pos = 0;
-    generateLastChunk();
-    
-    while (pos < value.size()){
-        HashForChunk(h0, h1, h2, h3, h4, pos);
-        pos += 64;
-    }
+    messageProcessing();
+
+    // 512 bit chunk consists of 64 characters, therefore:
+    for (size_t pos = 0; pos != value.size(); pos += 64)
+        hashForChunk(h0, h1, h2, h3, h4, pos);
+
     hashValue = hashLinker(h0, h1, h2, h3, h4);
 }
 
