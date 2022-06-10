d78812c3fdb446bcbaaacad64445d70efb67454f
NikitaMasych <92444221+NikitaMasych@users.noreply.github.com>
2022-06-10 11:05:36 +0300

    Update SHA1.h

--- Practice 3/SHA1.h
+++ Practice 3/SHA1.h
@@ -6,15 +6,15 @@
 class SHA1{
 
 private:
-    std::string hashValue;
-    std::vector <unsigned char> value;
+    std::string messageDigest;
+    std::vector <unsigned char> message;
     std::vector <uint_fast32_t> messageSchedule(const size_t& pos);
     void messageProcessing();
     void hashForChunk(uint_fast32_t& h0, uint_fast32_t&  h1,
                         uint_fast32_t& h2, uint_fast32_t&  h3,
                         uint_fast32_t& h4, const size_t& pos);
 public:
-    void enterValue();
+    void enterMessage();
     void calculateHash();
     std::string getHash();
 };
