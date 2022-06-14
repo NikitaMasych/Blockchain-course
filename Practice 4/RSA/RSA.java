c6509ba467e1339965d76cf2c303db87e97e7fab
NikitaMasych <92444221+NikitaMasych@users.noreply.github.com>
2022-06-14 12:14:23 +0300

    Update RSA.java

--- Practice 4/RSA/RSA.java
+++ Practice 4/RSA/RSA.java
@@ -11,8 +11,8 @@ public class RSA {
     private int hLen; // Length in octets of the specified hash algorithm
 
     /**
-     * Generates private (d) and public (e) exponents and modulus (n)
-     * @return BigInteger map of 3 values with d, e and n accordingly as char keys
+     * Generates private (d), public (e) exponents and modulus (n).
+     * @return BigInteger map of 3 values with d, e and n accordingly as char keys.
      */
     Map<Character, BigInteger> generateKeys() throws NoSuchAlgorithmException {
         BigInteger d, e, n;
@@ -30,11 +30,10 @@ public class RSA {
 
         return kS;
     }
-
     /**
-     * Requests key length in bits via console;
-     * Sets length in octets of the modulus n
-     * @param scanner represents object for input
+     * Requests key length in bits via console.
+     * Sets length in octets of the modulus n.
+     * @param scanner represents Scanner object for input.
      */
     void enterKeyLength(Scanner scanner) {
         System.out.println("Enter key length in bits: ");
@@ -46,32 +45,37 @@ public class RSA {
         else k = keyLength / 8;
         scanner.nextLine();
     }
-
     /**
-     * Requests hashing algorithm type via console
-     * until it is not valid
-     * Sets hash length in octets for specified algorithm
-     * @param scanner
+     * Calculates message digest length for a specified hashing algorithm.
+     * @param hashAlgorithm intended hashing algorithm.
+     * @return hash length.
+     * @throws NoSuchAlgorithmException
+     */
+    private static int calculateHashLength(String hashAlgorithm) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance(hashAlgorithm);
+        return md.digest("".getBytes(StandardCharsets.UTF_8)).length;
+    }
+    /**
+     * Requests hashing algorithm type via console until it is not valid.
+     * Sets hash length in octets for specified algorithm.
+     * @param scanner represents Scanner object for input.
      */
     void enterHashAlgorithm(Scanner scanner){
-
         System.out.println("Enter hashing algorithm: ");
         String hAlg = scanner.nextLine();
         try{
-            MessageDigest md = MessageDigest.getInstance(hAlg);
+            hLen = calculateHashLength(hAlg);
             hashAlgorithm = hAlg;
-            String str = "";
-            hLen = md.digest(str.getBytes(StandardCharsets.UTF_8)).length;
         }
         catch (NoSuchAlgorithmException e){
             System.out.println("Invalid hashing algorithm" + e);
             enterHashAlgorithm(scanner);
         }
     }
-
     /**
-     * Constructor for the RSA class;
-     * calls enterKeyLength() and enterHashAlgorithm() functions
+     * Constructor for the RSA class.
+     * Generates Scanner object with console input and
+     * calls enterKeyLength() and enterHashAlgorithm() functions.
      */
     RSA(){
         Scanner scanner = new Scanner(System.in);
@@ -79,40 +83,40 @@ public class RSA {
         enterHashAlgorithm(scanner);
     }
     /**
-     * Encryption function primitive
-     * @param e public exponent
-     * @param n public modulus
-     * @param m message representative, an integer between 0 and n - 1
-     * @return ciphertext representative, an integer between 0 and n - 1
+     * Encryption primitive.
+     * @param e public exponent.
+     * @param n public modulus.
+     * @param m message representative, an integer between 0 and n - 1.
+     * @return ciphertext representative, an integer between 0 and n - 1.
      */
-    public BigInteger RSAEP(BigInteger e, BigInteger n, BigInteger m){
-        if ((m.compareTo(BigInteger.ZERO) == -1) || (m.compareTo(n.subtract(BigInteger.ONE)) == 1)){
+    private static BigInteger RSAEP(BigInteger e, BigInteger n, BigInteger m){
+        if ((m.compareTo(BigInteger.ZERO) < 0) || (m.compareTo(n.subtract(BigInteger.ONE)) > 0)){
             throw new RuntimeException("Message representative out of range!");
         }
         return m.modPow(e, n);
     };
     /**
-     * Decryption function primitive
-     * @param d private exponent
-     * @param n public modulus
-     * @param c ciphertext representative, an integer between 0 and n - 1
-     * @return message representative, an integer between 0 and n - 1
+     * Decryption primitive.
+     * @param d private exponent.
+     * @param n public modulus.
+     * @param c ciphertext representative, an integer between 0 and n - 1.
+     * @return message representative, an integer between 0 and n - 1.
      */
-    public BigInteger RSADP(BigInteger d, BigInteger n, BigInteger c){
-        if ((c.compareTo(BigInteger.ZERO) == -1) || (c.compareTo(n.subtract(BigInteger.ONE)) != -1)){
+    private static BigInteger RSADP(BigInteger d, BigInteger n, BigInteger c){
+        if ((c.compareTo(BigInteger.ZERO) < 0) || (c.compareTo(n.subtract(BigInteger.ONE)) > -1)){
             throw new RuntimeException("Ciphertext representative out of range!");
         }
         return c.modPow(d, n);
     };
     /**
-     * Digital signature function primitive
-     * @param d private exponent
-     * @param n public modulus
-     * @param m message representative, an integer between 0 and n - 1
-     * @return signature representative, an integer between 0 and n - 1
+     * Digital signature primitive.
+     * @param d private exponent.
+     * @param n public modulus.
+     * @param m message representative, an integer between 0 and n - 1.
+     * @return signature representative, an integer between 0 and n - 1.
      */
-    public BigInteger RSASP1(BigInteger d, BigInteger n, BigInteger m){
-        if ((m.compareTo(BigInteger.ZERO) == -1) || (m.compareTo(n.subtract(BigInteger.ONE)) == 1)){
+    private static BigInteger RSASP1(BigInteger d, BigInteger n, BigInteger m){
+        if ((m.compareTo(BigInteger.ZERO) < 0) || (m.compareTo(n.subtract(BigInteger.ONE)) > 0)){
             throw new RuntimeException("Message representative out of range!");
         }
         return m.modPow(d, n);
@@ -124,60 +128,60 @@ public class RSA {
      * @param s signature representative, an integer between 0 and n - 1
      * @return message representative, an integer between 0 and n - 1
      */
-    public BigInteger RSAVP1(BigInteger e, BigInteger n, BigInteger s){
-        if ((s.compareTo(BigInteger.ZERO) == -1) || (s.compareTo(n.subtract(BigInteger.ONE)) > 0)){
+    private static BigInteger RSAVP1(BigInteger e, BigInteger n, BigInteger s){
+        if ((s.compareTo(BigInteger.ZERO) < 0) || (s.compareTo(n.subtract(BigInteger.ONE)) > 0)){
             throw new RuntimeException("Signature representative out of range!");
         }
         return s.modPow(e, n);
     }
     /**
-     * I2OSP converts a nonnegative integer to an octet string of a specified length.
-     * @params x nonnegative integer to be converted
-     * @params xLen intended length of the resulting octet string
-     * @return octet string of lenght xLen
+     * I2OSP converts a nonnegative integer into an octet string of a specified length.
+     * @params x nonnegative integer to be converted.
+     * @params xLen intended length of the resulting octet string.
+     * @return octet string of length xLen.
      */
-    public byte[] I2OSP(BigInteger x, int xLen){
-
-        if (x.compareTo(BigInteger.valueOf(256).pow(xLen)) != -1)
+    private static byte[] I2OSP(BigInteger x, int xLen){
+        if (x.compareTo(BigInteger.valueOf(256).pow(xLen)) > -1)
             throw new RuntimeException("Integer too large!");
 
         byte[] res = new byte[xLen];
         for(int i = 0; i != xLen; ++i){
             res[i] = (x.divide(BigInteger.valueOf(256).pow(xLen - i - 1)).byteValue());
         }
-
         return res;
     }
     /**
-     * OS2IP converts an octet string to a nonnegative integer.
-     * @params x octet string to be converted
-     * @return corresponding nonnegative integer
+     * OS2IP converts an octet string into a nonnegative integer.
+     * @params x octet string to be converted.
+     * @return corresponding nonnegative integer.
      */
-    public BigInteger OS2IP(byte[] x){
+    private static BigInteger OS2IP(byte[] x){
         BigInteger res = BigInteger.ZERO;
         for (int i = 0; i != x.length; ++i){
             res = res.add(BigInteger.valueOf(x[i] & 0xFF).multiply(BigInteger.valueOf(256).pow(x.length - i - 1)));
         }
         return res;
     }
-
     /**
-     * Hashing function
-     * @param str input message
-     * @return message digest as octet string
+     * Hashing function.
+     * @param str input message as an octet string.
+     * @param hashAlgorithm intended type of the hash algorithm.
+     * @return message digest as an octet string.
      */
-    public byte[] SHA(byte[] str) throws NoSuchAlgorithmException {
+    private static byte[] hashString(byte[] str, String hashAlgorithm) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance(hashAlgorithm);
         return md.digest(str);
     }
     /**
-     * Mask generation function
-     * @param Z seed from which mask is generated, an octet string
-     * @param l intended length in octets of the mask
-     * @return mask of l octets length
+     * Mask generation function.
+     * @param Z seed from which mask is generated, an octet string.
+     * @param l intended length in octets of the mask.
+     * @param hashAlgorithm intended type of the hash algorithm.
+     * @return mask of l octets length.
      */
-    byte[] MGF1(byte[] Z, int l) throws NoSuchAlgorithmException {
+    private static byte[] MGF1(byte[] Z, int l, String hashAlgorithm) throws NoSuchAlgorithmException {
 
+        int hLen = calculateHashLength(hashAlgorithm);
         if (l > Math.pow(2,hLen)) throw new RuntimeException("Mask too long!");
         byte[] T = new byte[l];
 
@@ -187,7 +191,7 @@ public class RSA {
         for (int counter = 0; counter != Math.ceilDiv(l,hLen) - 1; ++ counter){
             byte[] C = I2OSP(BigInteger.valueOf(counter), 4);
             System.arraycopy(C, 0, tmp, Z.length, 4);
-            byte[] tmp2 = SHA(tmp);
+            byte[] tmp2 = hashString(tmp, hashAlgorithm);
             int k = 0;
             while(i < l && k < hLen) {
                 T[i] = tmp2[k];
@@ -196,24 +200,23 @@ public class RSA {
         }
         return T;
     }
-
     /**
-     * Generates random octet string
-     * @param len intended length in octets of the seed
-     * @return seed string size of len
+     * Generates random octet string.
+     * @param len intended length in octets of the seed.
+     * @return seed string of size len.
      */
-    public byte[] seedRandom(int len) throws NoSuchAlgorithmException {
+    private static byte[] seedRandom(int len) throws NoSuchAlgorithmException {
         byte[] res = new byte[len];
         SecureRandom.getInstanceStrong().nextBytes(res);
         return res;
     }
     /**
-     * Applies xor operation to the corresponding characters of string a and b
-     * @param a denotes first term
-     * @param b denotes second term
-     * @return string of xor result
+     * Applies xor operation to the corresponding characters of string a and b.
+     * @param a denotes first term.
+     * @param b denotes second term.
+     * @return string of \xor result.
      */
-    byte[] XORStrings(byte[] a, byte[] b){
+    private static byte[] XORStrings(byte[] a, byte[] b){
         if (a.length != b.length)
             throw new RuntimeException("Lengths of strings diverges!");
         byte[] res = new byte[a.length];
@@ -223,25 +226,24 @@ public class RSA {
         return res;
     }
     /**
-     * Encryption operation
-     * Using specified hashing algorithm
-     * @param e public exponent
-     * @param n public modulus
-     * @param msg message to be encrypted, an octet string of length mLen
-     * @param l optional label to be associated with the message, by default, empty string
-     * @return ciphertext, an octet string of length k
+     * Encryption operation.
+     * Using specified hashing algorithm.
+     * @param e public exponent.
+     * @param n public modulus.
+     * @param msg message to be encrypted, an octet string of length mLen.
+     * @param l optional label to be associated with the message, by default, empty string.
+     * @return ciphertext, an octet string of length k.
      */
     public byte[] RSAES_OAEP_ENCRYPT(BigInteger e, BigInteger n, String msg, String l){
         int mLen = msg.length();
         if (mLen > k - 2*hLen - 2) throw new RuntimeException("Message too long!");
-        if (l == null) l = "";
 
         byte[] message = msg.getBytes(StandardCharsets.UTF_8);
         byte[] L = l.getBytes(StandardCharsets.UTF_8);
 
         // EME-OAEP Encoding:
         try {
-            byte[] lHash = SHA(L);
+            byte[] lHash = hashString(L, hashAlgorithm);
 
             byte[] DB = new byte[k - hLen - 1];
             System.arraycopy(lHash, 0, DB, 0, hLen);
@@ -252,9 +254,9 @@ public class RSA {
             System.arraycopy(message, 0, DB, DB.length - mLen, mLen);
 
             byte[] seed = seedRandom(hLen);
-            byte[] dbMask = MGF1(seed, k - hLen - 1);
+            byte[] dbMask = MGF1(seed, k - hLen - 1, hashAlgorithm);
             byte[] maskedDB = XORStrings(DB, dbMask);
-            byte[] seedMask = MGF1(maskedDB, hLen);
+            byte[] seedMask = MGF1(maskedDB, hLen, hashAlgorithm);
             byte[] maskedSeed = XORStrings(seed, seedMask);
 
             byte[] EM = new byte[k];
@@ -275,14 +277,18 @@ public class RSA {
 
         return message;
     }
+    // piece of overloading to achieve default associated label L
+    public byte[] RSAES_OAEP_ENCRYPT(BigInteger e, BigInteger n, String msg){
+            return RSAES_OAEP_ENCRYPT(e ,n, msg,"");
+    }
     /**
-     * Decryption operation
-     * Using SHA3-256
-     * @param d private exponent
-     * @param n public modulus
-     * @param ciphertext ciphertext to be decrypted, an octet string of length k
-     * @param l optional label whose association with the message
-     * @return message, an octet string of length mLen, where mLen <= k - 2hLen - 2
+     * Decryption operation.
+     * Using specified hashing algorithm.
+     * @param d private exponent.
+     * @param n public modulus.
+     * @param ciphertext ciphertext to be decrypted, an octet string of length k.
+     * @param l optional label associated with the message.
+     * @return message, an octet string of length mLen, where mLen <= k - 2hLen - 2.
      */
     public byte[] RSAES_OAEP_DECRYPT(BigInteger d, BigInteger n, byte[] ciphertext, String l){
         try {
@@ -294,14 +300,14 @@ public class RSA {
             // EME-OAEP Decoding:
             byte[] EM = I2OSP(m, k);
             if (l == null) l = "";
-            byte[] lHash = SHA(l.getBytes(StandardCharsets.UTF_8));
+            byte[] lHash = hashString(l.getBytes(StandardCharsets.UTF_8), hashAlgorithm);
             byte Y = EM[0];
 
             byte[] maskedSeed = Arrays.copyOfRange(EM, 1, hLen+1);
             byte[] maskedDB = Arrays.copyOfRange(EM, hLen+1, k);
-            byte[] seedMask = MGF1(maskedDB, hLen);
+            byte[] seedMask = MGF1(maskedDB, hLen, hashAlgorithm);
             byte[] seed = XORStrings(maskedSeed, seedMask);
-            byte[] dbMask = MGF1(seed, k - hLen - 1);
+            byte[] dbMask = MGF1(seed, k - hLen - 1, hashAlgorithm);
             byte[] DB = XORStrings(maskedDB, dbMask);
             byte[] lHash1 = Arrays.copyOfRange(DB, 0, hLen);
 
@@ -322,6 +328,10 @@ public class RSA {
         }
 
         return ciphertext;
+    }
+    //overloading to achieve default empty associated label
+    public byte[] RSAES_OAEP_DECRYPT(BigInteger d, BigInteger n, byte[] ciphertext){
+        return RSAES_OAEP_DECRYPT(d,n,ciphertext, "");
     }
 
 }
