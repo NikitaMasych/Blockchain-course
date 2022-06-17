1fbcd372e8c7e4d1cc1cdfc4ab1f7ec3b6cf56db
NikitaMasych <92444221+NikitaMasych@users.noreply.github.com>
2022-06-17 12:34:38 +0300

    Update AES.h

--- Practice 4/AES/AES.h
+++ Practice 4/AES/AES.h
@@ -6,30 +6,28 @@
 
 class AES{
 private:
-    std::vector <uint_fast8_t> plaintext, ciphertext, key;
-    std::vector <uint_fast32_t> words; // expanded key
+    std::vector <uint_fast8_t> plaintext, ciphertext, decryptedCiphertext, key;
     size_t keyLen;
     size_t Nk, Nr;
-    std::vector<std::vector<uint_fast8_t> > sBox;
+    std::vector<std::vector<uint_fast8_t> > sBox, invSBox;
     void enterKey();
     void enterKeyLength();
     void initializeSBox();
+    void initializeInvSBox();
     void keyExpansion();
     void subWord(std::vector<uint_fast8_t> &temp);
-    std::vector<std::vector<uint_fast8_t> > calculateBlock(size_t pos);
-    void calculateCiphertext(
-         std::vector <std::vector<std::vector<uint_fast8_t> > > cipher);
     void addRoundKey(std::vector<std::vector<uint_fast8_t> > &state,
                      size_t pos);
     void subBytes(std::vector<std::vector<uint_fast8_t> > &state);
+    void invSubBytes(std::vector<std::vector<uint_fast8_t> > &state);
 public:
     void requestKey();
     void enterPlaintext();
     void enterPlaintextAsHex();
     void encryptPlaintext();
     void decryptCiphertext();
-    void outputPlaintext();
     void outputCiphertext();
+    void outputDecryptedCiphertext();
     void outputExpandedKeys();
     std::vector <uint_fast8_t> getPlaintext();
     std::vector <uint_fast8_t> getCiphertext();
