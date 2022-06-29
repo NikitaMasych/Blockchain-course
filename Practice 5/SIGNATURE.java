741f8835b24d04aab478f1f801400c14cda7a010
NikitaMasych <92444221+NikitaMasych@users.noreply.github.com>
2022-06-29 12:56:10 +0300

    Update SIGNATURE.java

--- Practice 5/SIGNATURE.java
+++ Practice 5/SIGNATURE.java
@@ -1,31 +1,31 @@
-import java.nio.charset.StandardCharsets;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.Signature;
+    import java.nio.charset.StandardCharsets;
+    import java.security.PrivateKey;
+    import java.security.PublicKey;
+    import java.security.Signature;
 
-public class SIGNATURE{
-    public static byte[] signData(PrivateKey prk, String message){
-        try {
-            Signature signature = Signature.getInstance("SHA3-256withDSA");
-            signature.initSign(prk);
-            signature.update(message.getBytes(StandardCharsets.UTF_8));
-            return signature.sign();
+    public class SIGNATURE{
+        public static byte[] signData(PrivateKey prk, String message){
+            try {
+                Signature signature = Signature.getInstance("SHA-256withECDSA");
+                signature.initSign(prk);
+                signature.update(message.getBytes(StandardCharsets.UTF_8));
+                return signature.sign();
+            }
+            catch (Exception e){
+                System.out.println(e);
+            }
+            return message.getBytes(StandardCharsets.UTF_8);
         }
-        catch (Exception e){
-            System.out.println(e);
+        public static boolean verifySignature(byte[] signedMessage, String message, PublicKey pbk){
+            try {
+                Signature signature = Signature.getInstance("SHA-256withECDSA");
+                signature.initVerify(pbk);
+                signature.update(message.getBytes(StandardCharsets.UTF_8));
+                return signature.verify(signedMessage);
+            }
+            catch (Exception e){
+                System.out.println(e);
+            }
+            return false;
         }
-        return message.getBytes(StandardCharsets.UTF_8);
     }
-    public static boolean verifySignature(byte[] signedMessage, String message, PublicKey pbk){
-        try {
-            Signature signature = Signature.getInstance("SHA3-256withDSA");
-            signature.initVerify(pbk);
-            signature.update(message.getBytes(StandardCharsets.UTF_8));
-            return signature.verify(signedMessage);
-        }
-        catch (Exception e){
-            System.out.println(e);
-        }
-        return false;
-    }
-}
