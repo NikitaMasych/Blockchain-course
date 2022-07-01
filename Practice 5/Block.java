c7e5ebf39b3dbd8642df0388015f7d7587cb99fa
NikitaMasych <92444221+NikitaMasych@users.noreply.github.com>
2022-07-02 01:49:49 +0300

    Update Block.java

--- Practice 5/Block.java
+++ Practice 5/Block.java
@@ -6,6 +6,12 @@ public class Block {
     public byte[] prevHash;
     public ArrayList<Transaction> setOfTransactions;
 
+    /**
+     * Creates block.
+     * Calculates it's blockID.
+     * @return created of all transactions and specified previous hash block.
+     * @throws Exception
+     */
     public static Block createBlock(ArrayList<Transaction> setOfTransactions, byte[] prevHash) throws Exception {
         Block block = new Block();
         block.setOfTransactions = setOfTransactions;
@@ -13,6 +19,13 @@ public class Block {
         block.blockID = calculateBlockID(prevHash, setOfTransactions);
         return block;
     }
+    /**
+     * Calculates blockID in a matter of hash.
+     * @param prevHash previous block hash.
+     * @param setOfTransactions all transactions that are going to be included in the block.
+     * @return blockID, calculated basing on transactions hash-chain and previous block hash.
+     * @throws Exception
+     */
     private static byte[] calculateBlockID(byte[] prevHash, ArrayList<Transaction> setOfTransactions)
             throws Exception {
         byte[] digest = setOfTransactions.get(0).transactionID;
