c1cd20524034a11997750ef56316c14708116471
NikitaMasych <92444221+NikitaMasych@users.noreply.github.com>
2022-07-01 22:00:59 +0300

    Update SIGNATURE.java

--- Practice 5/SIGNATURE.java
+++ Practice 5/SIGNATURE.java
@@ -1,31 +1,20 @@
-    import java.nio.charset.StandardCharsets;
-    import java.security.PrivateKey;
+ import java.security.PrivateKey;
     import java.security.PublicKey;
     import java.security.Signature;
 
     public class SIGNATURE{
-        public static byte[] signData(PrivateKey prk, String message){
-            try {
-                Signature signature = Signature.getInstance("SHA-256withECDSA");
-                signature.initSign(prk);
-                signature.update(message.getBytes(StandardCharsets.UTF_8));
-                return signature.sign();
-            }
-            catch (Exception e){
-                System.out.println(e);
-            }
-            return message.getBytes(StandardCharsets.UTF_8);
+        public static byte[] signData(PrivateKey prk, byte[] message) throws  Exception{
+            Signature signature = Signature.getInstance("SHA-256withECDSA");
+            signature.initSign(prk);
+            signature.update(message);
+            return signature.sign();
         }
-        public static boolean verifySignature(byte[] signedMessage, String message, PublicKey pbk){
-            try {
-                Signature signature = Signature.getInstance("SHA-256withECDSA");
-                signature.initVerify(pbk);
-                signature.update(message.getBytes(StandardCharsets.UTF_8));
-                return signature.verify(signedMessage);
-            }
-            catch (Exception e){
-                System.out.println(e);
-            }
-            return false;
+        public static boolean verifySignature(byte[] signedMessage, byte[] message, PublicKey pbk)
+                throws Exception{
+            Signature signature = Signature.getInstance("SHA-256withECDSA");
+            signature.initVerify(pbk);
+            signature.update(message);
+            return signature.verify(signedMessage);
+
         }
     }
